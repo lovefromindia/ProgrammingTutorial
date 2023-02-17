a6a3bef145b1f98d8d48c309bc016c606fc1c88c
Mihir <mahir3.14pie@gmail.com>
2023-02-17 19:20:44 +0530

    switch case

--- LearningConditionStatements.java
+++ LearningConditionStatements.java
@@ -1,3 +1,26 @@
 public class LearningConditionStatements {
 
+    public static void main(String[] args) {
+
+        String currentCourse = "java";
+
+        if (currentCourse.equals("java")) {
+
+            System.out.println("Hello to Java");
+
+        }
+
+//        below line will give error as java only works with boolean in control statements and cannot converts int to boolean
+//        else if(1){
+//
+//        }
+
+        else{
+
+            System.out.println("Still doing Advance Networking");
+
+        }
+
+    }
+
 }
---
+++ LearningSwitchCase.java
@@ -0,0 +1,35 @@
+public class LearningSwitchCase {
+
+    public static void main(String[] args) {
+
+        String name = "Java";
+
+        switch(name){
+
+            case "Cpp":
+
+                System.out.println("Cool Language");
+
+                break;
+
+            case "C":
+
+                System.out.println("Old but Gold");
+
+                break;
+
+            case "Jav":
+
+                System.out.println("Good one");
+
+                break;
+
+            default:
+
+                System.out.println("Don't say HTML is a programming language");
+
+        }
+
+    }
+
+}
