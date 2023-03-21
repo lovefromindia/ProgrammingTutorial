0dbd8eb72c6b6ad79f7ea011957e4f88ec75b8d8
Mihir <mahir3.14pie@gmail.com>
2023-03-21 19:09:02 +0530

    march 22PingApp

---
+++ com/developing/Main.java
@@ -0,0 +1,8 @@
+package com.developing;
+
+public class Main {
+    public static void main(String[] args) {
+        PingApp pingApp = new PingApp();
+        pingApp.checkStatus("10.20.40.234");
+    }
+}
--- com/developing/PingApp.java
+++ com/developing/PingApp.java
@@ -3,146 +3,64 @@ package com.developing;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class PingApp {
-
-    private static final int DEFAULT_RETRY = 5;
-    private static final int MIN_RETRY = 0;
-    private static final int MAX_RETRY = 20;
-
-
-
-    private static final int DEFAULT_RETRYINTERVAL = 25;
-    private static final int MIN_RETRYINTERVAL = 15;
-    private static final int MAX_RETRYINTERVAL = 1000;
-
-    private static final int DEFAULT_RESPONSETIMEOUT = 500;
-    private static final int MIN_RESPONSETIMEOUT = 10;
-    private static final int MAX_RESPONSETIMEOUT = 1000;
-
-
-    /**
-     * Private Static Class which actually checks whether the host is up or down
-     * contains checkIfUp() method which takes four values: ip, numberofretries, timeintervalbetweenretries
-     */
-    private static class PingExecutor {
-
-        public static boolean checkIfUp(String targetIp,int numberOfRetry,int retryInterval,int responseTimeOut) {
-
-            BufferedReader reader = null;
-
-            ProcessBuilder processBuilder = null;
-
-            ArrayList<String> commandList = null;
-
-            String[] command = null;
-
-            Process process = null;
-
-            StringBuilder pingOutput = null;
-
-            try{
-
-                processBuilder = new ProcessBuilder();
-
-                 commandList = new ArrayList<>();
-
-                commandList.add("fping");
-
-                commandList.add("-r" + numberOfRetry);
-
-                commandList.add("-i" + retryInterval);
-
-                commandList.add("-t" + responseTimeOut);
-
-                commandList.add(targetIp);
-
-                command = commandList.toArray(new String[commandList.size()]);
-
-                //for logging
-                System.out.println(Arrays.toString(command));
-
-                processBuilder.command(command);
-
-                processBuilder.redirectErrorStream(true);
-
-                process = processBuilder.start();
-
-                reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-                int pingCommandExitCode = process.waitFor();
-
-                if(pingCommandExitCode != 0 && pingCommandExitCode != 1){
-
-                    throw new Exception("Ping Process has been exited with code " + pingCommandExitCode);
-
-                }
-
-                pingOutput = new StringBuilder();
-
-                String line;
-
-                while ((line = reader.readLine()) != null) {
-
-                    pingOutput.append(line);
-
-                }
-
-                //for logging
-                System.out.println("Ping: " + pingOutput.toString());
-
-                return pingOutput.toString().contains("alive");
-
-            }catch(Exception exception){
-
-                System.out.println(exception.getMessage());
-
-            }finally {
-
-                try {
-
-                    reader.close();
-
-                } catch (IOException exception) {
-
-                    System.out.println(exception.getMessage());
-
-                }
-
-            }
-
-            System.out.println("Try again...");
-
-            return false;
-
-        }
-
-    }
-
-    public static void main(String[] args) {
-
-        String targetIp;
-
-        int retry=DEFAULT_RETRY,retryInterval=DEFAULT_RETRYINTERVAL,responseTimeOut=DEFAULT_RESPONSETIMEOUT;
+    public final int checkStatus(String ip){
 
         try {
+            if (!validateIP(ip)) {
+                return -1;
+            }
 
-            BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
+            ProcessBuilder builder = new ProcessBuilder();
+            builder.command(getCommand(ip));
+            builder.redirectErrorStream(true);
 
-            System.out.println("Enter IP Address");
+            Process process = builder.start();
+            int exitCode = process.waitFor();
+            if(exitCode != 0 && exitCode != 1){
+                return -1;
+            }
 
-            targetIp = userInputReader.readLine();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder result = new StringBuilder();
+            String line;
+            while((line=reader.readLine()) != null){
+                result.append(line);
+            }
 
-            System.out.println(PingExecutor.checkIfUp(targetIp,retry,retryInterval,responseTimeOut));
+            Map<String,String> results = new HashMap<>();
+            Arrays.stream(result.toString().split(":")[1].split(",")).forEach(s -> {
+                String[] keyValues = s.split("=");
+                if(keyValues.length==2){
+                    String[] keys = keyValues[0].split("/");
+                    String[] values = keyValues[1].split("/");
+                    for (int i = 0; i < keys.length; i++) {
+                        results.put(keys[i],values[i]);
+                    }
+                }
+            });
+            return ;
 
-        } catch (Exception exception) {
-
+        }catch(Exception exception){
             System.out.println(exception.getMessage());
-
+            return -1;
         }
+    }
 
+    private List<String> getCommand(String ip) {
+        List<String> command = new ArrayList<>();
+        command.add("fping");
+        command.add("-c3");
+        command.add("-q");
+        command.add(ip);
+        return command;
+    }
+
+    private final boolean validateIP(String ip){
+        return true;
     }
 
 }
